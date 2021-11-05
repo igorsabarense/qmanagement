a1d52ce0810c1f3fcdd28dacbe9b3c7d82f94a68
Igor Sabarense <igor.sabarense@gmail.com>
2021-11-05 18:25:41 -0300

    "initial commit"

---
+++ src/components/homeCliente.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import Login from './login';
+import App.css
+
+class HomeGerencia extends Component{
+    render(){
+        return <div className="">
+            <Login nome={"Gerência"}/>
+        </div>
+    }
+}
+
+export default HomeGerencia;
